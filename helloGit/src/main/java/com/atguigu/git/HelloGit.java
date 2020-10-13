6b7561bdd2d0ffc1ae4342ae130d7b599a6ae7c3
lijingzang <3221513858@qq.com>
2020-10-13 18:18:20 +0800

    添加文件

---
+++ helloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+import sun.applet.Main;
+
+/**
+ * @author ljz on 2020/10/13
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello GIt!");
+    }
+}
